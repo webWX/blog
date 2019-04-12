cf514ebd80cea3c91b7cb7f246a9369a4f57b9cf
wangxiong <xiong.wang@dolabank.com>
2019-04-12 17:49:03 +0800

    新增:  1. vuex -- state

---
+++ vue/vuex/state.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>vuex--state</section>
+</template>
+
+<script>
+    import { mapState } from 'vue';
+    import { createNamespacedHelpers } from 'vuex';
+
+    const { mapState: mapStateProduct } = createNamespacedHelpers('product');
+
+    export default {
+        computed: {
+            ...mapState(['count']),
+
+            ...mapState({
+                count: state => state.count,
+
+                countAlias: 'count'
+            }),
+
+            ...mapState('product', {
+                count: state => state.count
+            }),
+
+            ...mapStateProduct({
+                count: state => state.count
+            })
+        }
+    }
+</script>
